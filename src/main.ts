510aa6240ff93374326015a1af6daafb0bf709e2
eeuneeun <sacroo89@gmail.com>
2025-09-12 23:52:44 +0900

    ip추가

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:3030',
       'http://34.158.210.111:3000',
+      'http://34.22.104.204:3000',
       'http://34.158.207.71:3030',
     ], // Next.js (프론트) 주소
     credentials: true, // 쿠키/인증정보 허용할 경우
